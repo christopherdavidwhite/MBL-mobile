08695de5612108fb1a0c327ddb44ef874eaa15f9
Christopher White <christopher.white@alumni.rice.edu>
2018-01-22 10:20:15 -0800

    finite-speed time evolution with a variety of integration schemes and trotter decompositions

--- time_evolution.jl
+++ time_evolution.jl
@@ -30,10 +30,31 @@ function timestep(bond_evals :: Array{Float64,1},
                   bond_evects :: Array{Complex{Float64},2},
                   field :: Array{Float64, 1},
                   h :: Float64,
+                  J :: Float64,
                   δ :: Float64,
-                  U :: Array{Complex{Float64}, 2})
-    expm_bond = scale(bond_evects, exp(-im*δ*bond_evals)) * bond_evects'
-    U = scale(expm_bond, exp(-im*h*δ*field)) * U
+                  U :: Array{Complex{Float64}, 2},
+                  Y :: Array{Complex{Float64}, 2}, #storage for multiplication results
+                  trotter_order :: Number,)
+    if trotter_order == 1
+        Ac_mul_B!(Y, bond_evects,  U)
+        scale!(exp.(-im*δ*J*bond_evals), Y)
+        A_mul_B!(U, bond_evects, Y)
+        scale!(exp.(-im*δ*h*field), U)
+    elseif trotter_order == 2
+        expfield_2 = exp.(-im*δ*h*field/2)
+        scale!(expfield_2, U)
+        Ac_mul_B!(Y, bond_evects,  U)
+        scale!(exp.(-im*δ*J*bond_evals), Y)
+        A_mul_B!(U, bond_evects, Y)
+        scale!(expfield_2, U)
+    elseif trotter_order == Inf
+        N = size(Y,1)
+        #don't play the allocation game here: will be overwhelmed by expm1
+        H = bond_evects * diagm(J*bond_evals) * bond_evects' + diagm(h*field)
+        U = expm(-im*δ*H)*U
+    else
+        error("unsupported trotter order $(trotter_order)")
+    end
     U :: Array{Complex{Float64}, 2}
     return U
 end
@@ -56,37 +77,121 @@ function backwardalpha_timeevolution(sys :: AbstractSpinHalfChain, T :: Float64,
     return U
 end
 
-function forwardalpha_timeevolution(sys :: RFHeis, T :: Float64, δ :: Float64)
-    U = eye(Complex{Float64}, 2^sys.L)
+function forwardalpha_timeevolution(sys :: RFHeis,
+        T :: Float64,
+        δ :: Float64,
+        trotter_order :: Number = Inf,
+        integrator_order = 2,
+    )
+        
+    N = size(sys.Z[1],1)
+    U = eye(Complex{Float64}, N)
+    Y = zeros(Complex{Float64}, N,N)
     hfn = sys.h
     Qfn = sys.scale
     bond_evals = sys.bond_evals
     bond_evects = sys.bond_evects
     field = sys.field
-    for t in 0:δ:(T-δ)
-        h = (hfn(t/T) :: Float64)
-        Q = (Qfn(h) :: Float64)
-        U = timestep(bond_evals, bond_evects, field, h, δ*Q, U)
+    for t in 0:δ:T-δ
+        if integrator_order == 1
+            Qinv = sys.scale(t/T)
+            J = Qinv
+            h = Qinv * sys.h(t/T)
+        elseif integrator_order == 2
+            Qinv1 = sys.scale(t/T)
+            Qinv2 = sys.scale((t-δ)/T)
+            J1 = Qinv1
+            J2 = Qinv2
+
+            h1 = Qinv1 * sys.h(t/T)
+            h2 = Qinv2 * sys.h((t-δ)/T)
+            
+            J = 0.5*(J1 + J2)
+            h = 0.5*(h1 + h2)
+        elseif integrator_order == 4
+            error("this is not actually a fourth-order integrator")
+            Qinv1 = sys.scale(t/T)
+            Qinv2 = sys.scale((t - δ/2)/T)
+            Qinv3 = sys.scale((t - δ)/T)
+            
+            J1 = Qinv1
+            J2 = Qinv2
+            J3 = Qinv3
+            
+            h1 = Qinv1 * sys.h(t/T)
+            h2 = Qinv2 * sys.h((t - δ/2)/T)
+            h3 = Qinv3 * sys.h((t - δ)/T)
+            
+            h = 1/6 * (h1 + 4*h2 + h3)
+            J = 1/6 * (J1 + 4*J2 + J3)
+        else
+            error("no supported integrator at order $integrator_order")
+        end
+        U = timestep(bond_evals, bond_evects, field, h, J, δ, U, Y, trotter_order)
     end
     update!(sys, 1.0)
     return U
 end
 
-function backwardalpha_timeevolution(sys :: RFHeis, T :: Float64, δ :: Float64)
-    U = eye(Complex{Float64}, 2^sys.L)
+function backwardalpha_timeevolution(sys :: RFHeis,
+        T :: Float64,
+        δ :: Float64,
+        trotter_order :: Number = Inf,
+        integrator_order = 2,
+    )
+        
+    N = size(sys.Z[1],1)
+    U = eye(Complex{Float64}, N)
+    Y = zeros(Complex{Float64}, N,N)
     hfn = sys.h
     Qfn = sys.scale
     bond_evals = sys.bond_evals
     bond_evects = sys.bond_evects
     field = sys.field
     for t in T:-δ:δ
-        h = (hfn(t/T) :: Float64)
-        Q = (Qfn(h) :: Float64)
-        U = timestep(bond_evals, bond_evects, field, h, δ*Q, U)
+        if integrator_order == 1
+            Qinv = sys.scale(t/T)
+            J = Qinv
+            h = Qinv * sys.h(t/T)
+        elseif integrator_order == 2
+            Qinv1 = sys.scale(t/T)
+            Qinv2 = sys.scale((t-δ)/T)
+            J1 = Qinv1
+            J2 = Qinv2
+
+            h1 = Qinv1 * sys.h(t/T)
+            h2 = Qinv2 * sys.h((t-δ)/T)
+            
+            J = 0.5*(J1 + J2)
+            h = 0.5*(h1 + h2)
+        elseif integrator_order == 4
+            error("this is not actually a fourth-order integrator")
+            Qinv1 = sys.scale(t/T)
+            Qinv2 = sys.scale((t - δ/2)/T)
+            Qinv3 = sys.scale((t - δ)/T)
+            
+            J1 = Qinv1
+            J2 = Qinv2
+            J3 = Qinv3
+            
+            h1 = Qinv1 * sys.h(t/T)
+            h2 = Qinv2 * sys.h((t - δ/2)/T)
+            h3 = Qinv3 * sys.h((t - δ)/T)
+            
+            h = 1/6 * (h1 + 4*h2 + h3)
+            J = 1/6 * (J1 + 4*J2 + J3)
+        else
+            error("no supported integrator at order $integrator_order")
+        end
+        U = timestep(bond_evals, bond_evects, field, h, J, δ, U, Y, trotter_order)
     end
     update!(sys, 0.0)
     return U
 end
+
+
+
+
 
 #This particular use of multiple dispatch (T as string for
 #less-than-straightforward, e.g. adiabatic, time evolution) might be
